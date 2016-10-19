e6a13fe5e22083c96f8233f8a238d76e5eec97f4
Zahra Ghanbari <ghanbarifam@gmail.com>
2016-10-19 15:56:30 +0200

    books folder

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ app.get('/', function(request, response) {
   response.send(pug.renderFile(__dirname + '/views/index.pug', {}));
   // response.sendFile(__dirname + '/index.html');
 });
+app.get('books/if-you-could-mine', function(request, response) {
+  console.log('Requesting contact page....');
+  response.send(pug.renderFile(__dirname + '/views/books/if-you-could-mine.pug', {}));
+});
+app.get('books/the-little-prince', function(request, response) {
+  console.log('Requesting contact page....');
+  response.send(pug.renderFile(__dirname + '/views/books/the-little-prince.pug', {}));
+});
+app.get('books/the-girl-on-the-train', function(request, response) {
+  console.log('Requesting contact page....');
+  response.send(pug.renderFile(__dirname + '/views/books/the-girl-on-the-train.pug', {}));
+});
+
+
 app.listen(3000, function() {
   console.log('Web server started on port 3000');
 });
